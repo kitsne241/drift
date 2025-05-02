6334a0d77f3776960af6a9c7bd3ad1efb10f0f32
kitsne <bicycle.y.t@gmail.com>
2025-05-02 22:39:41 +0900

    ファイルの整理と補足

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
+
+// とりあえず prettier 公式サイトから引っ張ってきた設定
--- react-router.config.ts
+++ react-router.config.ts
@@ -5,3 +5,7 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
 } satisfies Config;
+
+// 最新の React Router はデフォルトで SSR をサポートしている（どころか、有効にしている）
+// Vue は Vue + Nuxt で SSR をサポートしているが、React なら React Router だけで SSR が可能
+// ただし、Next.js は他にもいろいろ機能があるのでお役御免になるわけではない
