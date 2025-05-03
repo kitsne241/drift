c90a5aa2a2165f637fecbb0d3a94f0fcf733b8bc
kitsne <bicycle.y.t@gmail.com>
2025-05-03 10:37:14 +0900

    設定ファイルの補足とエラー解消

--- prettier.config.js
+++ prettier.config.js
@@ -7,8 +7,13 @@ const config = {
   tabWidth: 4,
   semi: false,
   singleQuote: true,
-};
+}
 
-export default config;
+export default config
 
 // とりあえず prettier 公式サイトから引っ張ってきた設定
+
+// 冒頭のコメントは JSDoc コメントというもので、JavaScript のオブジェクトに型注釈を行うもの
+// TypeScript では専ら型を定義するための JSDoc は必要ないが、
+// 他にもオブジェクトの役割の説明など、コードに注釈をするために依然としてよく使われている
+// JSDoc コメントは定義の直前に改行を入れずに書くのが正しい書き方とされる
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from "@react-router/dev/config"
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-} satisfies Config;
+} satisfies Config
 
-// 最新の React Router はデフォルトで SSR をサポートしている（どころか、有効にしている）
+// 最新の React Router は SSR をサポートしている（どころか、デフォルトで有効にしている）
 // Vue は Vue + Nuxt で SSR をサポートしているが、React なら React Router だけで SSR が可能
 // ただし、Next.js は他にもいろいろ機能があるのでお役御免になるわけではない
